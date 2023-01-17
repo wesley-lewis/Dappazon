32df0f4728df8a4c4cd1f56ba1b6be986ae8e725
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-17 21:50:20 +0530

    Added the list function along with some basic tests

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+  string public name;
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  // List products
+  function list() public {
+    
+  }
+  // Buy products
+
+  // Withdraw funds
+
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,27 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+  let dappazon, deployer, buyer
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners()
+    console.log(deployer.address, buyer.address)
+
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+
+  })
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+  })
 
 })

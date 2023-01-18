2c0832cc7ed566bc3d00a90999191d89b972bc3d
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-18 21:21:38 +0530

    Finished the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,7 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+const { ethers } = require("hardhat")
 const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
@@ -12,6 +13,29 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+
+  // List items
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
 
 }
 

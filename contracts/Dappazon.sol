9734ce951b99c3cc516facc96ef5fade1463ea40
wesley-lewis <lewis.wes.23@gmail.com>
2023-01-18 19:33:16 +0530

    Finished the buy, withdraw functions

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -5,16 +5,92 @@ contract Dappazon {
   string public name;
   address public owner;
 
+  struct Item {
+    uint256 _id;
+    string name;
+    string category;
+    string image;
+    uint256 cost;
+    uint256 rating;
+    uint256 stock;
+  }
+
+  struct Order {
+    uint256 time;
+    Item item;
+  }
+
+  event List(string name, uint256 cost, uint256 quantity);
+  event Buy(address buyer, uint256 orderId, uint256 itemId);
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  mapping(uint256 => Item) public items;
+  mapping(address => uint256) public orderCount;
+  mapping(address => mapping(uint256 => Order)) public orders;
+
   constructor() {
     owner = msg.sender;
   }
 
   // List products
-  function list() public {
+  function list(
+  uint256 _id, 
+  string memory _name, 
+  string memory _category,
+  string memory _image,
+  uint256 _cost,
+  uint256 _rating,
+  uint256 _stock 
+  ) public onlyOwner{
     
+    // Create Item struct
+    Item memory item = Item(
+      _id,
+      _name, 
+      _category, 
+      _image, 
+      _cost, 
+      _rating, 
+      _stock
+      );
+
+    // Save Item struct
+    items[_id] = item;
+
+    // Emit an event
+    emit List(_name, _cost, _stock);
   }
   // Buy products
 
+  function buy(uint256 _id) public payable{
+    // Fetch item
+    Item memory item = items[_id];
+
+    require(msg.value >= item.cost);
+
+    require(item.stock > 0);
+
+    // Create an order
+    Order memory order = Order(block.timestamp, item);
+
+    // Save order to chain
+    orderCount[msg.sender]++;
+    orders[msg.sender][orderCount[msg.sender]] = order;
+    // Substrack stock
+    items[_id].stock = item.stock - 1;
+
+    // Emit event
+    emit Buy(msg.sender, orderCount[msg.sender], item._id);
+  }
+
   // Withdraw funds
+  function withdraw() public onlyOwner {
+    (bool success, ) = owner.call{value: address(this).balance}("");
+    require(success);
+  }
 
 }
